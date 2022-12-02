a0ebbe57fb654501735ef6ea97377f3e6685ad07
Thiago Menezes <107003617+thmenezes2006@users.noreply.github.com>
2022-12-02 11:06:35 -0300

    teste book.service

--- src/books/services/book.service.spec.ts
+++ src/books/services/book.service.spec.ts
@@ -18,7 +18,37 @@ describe("BookService", () => {
             expect(books).toEqual(fakeBookData)
         })
         it("Deve retornar uma promessa de Erro", async () => {
-            jest.spyOn(fakeBookRepository)
+            jest.spyOn(fakeBookRepository, "getAll").mockRejectedValueOnce("Error")
+            const error = await bookService.getAll()
+
+            expect(error).toEqual({
+                promiseError: {
+                    message: "Requisição do banco de dados não completada",
+                    error: "Error",
+                }
+            })
+        })
+    })
+
+    describe("getById", () => {
+        it("Deve se conectar em Repository.getById", async () => {
+            const spy = jest.spyOn(fakeBookRepository, "getById")
+            await bookService.getById(fakeId)
+            expect(spy).toHaveBeenCalled()
+        })
+        it("Deve retornar um livro", async () => {
+            const book = await bookService.getById(fakeId)
+            expect(book).toEqual(fakeBookData[0])
+        })
+        it("Deve retornar uma promessa de erro", async () => {
+            jest.spyOn(fakeBookRepository, "getById").mockRejectedValueOnce("Error")
+            const error = await bookService.getById(fakeId)
+            expect(error).toEqual({
+                promisseError: {
+                    message:"Requisição ao banco de dados não completada!",
+                    error: "Error"
+                }
+            })
         })
     })
 })
